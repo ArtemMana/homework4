44f2ae51c5d5db45ca526c024892af867ad8cd8a
Artem Manakov <artem.manakov0@gmail.com>
2020-08-17 18:03:33 +0300

    assignment3-task4

--- ДЗ№4.1/ViewController.swift
+++ ДЗ№4.1/ViewController.swift
@@ -14,12 +14,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         block4mission1()
+        block4mission4()
     }
     func block4mission1() {
         let name = "Artem"
         print("Меня зовут \(name)")
 
         print("Количество символов в моем имени \(name.count)")
+    }
+    
+    func block4mission4() {
+        var word = "hello"
+        print(word)
+        var mirrorWord = String()
+        for _ in 0..<word.count {
+            var endLetter = word.remove(at: word.index(before: word.endIndex))
+            mirrorWord += String(endLetter)
+        }
+        print(mirrorWord)
     }
 }
 
