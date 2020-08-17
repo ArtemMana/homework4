a4705317fd847528a90f338f92fdbb11165a67d9
Artem Manakov <artem.manakov0@gmail.com>
2020-08-17 17:58:51 +0300

    Initial Commit

---
+++ ДЗ№4.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ДЗ№4.1
+//
+//  Created by Artem Manakov on 17.08.2020.
+//  Copyright © 2020 Artem Manakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
