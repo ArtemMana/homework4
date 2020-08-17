7c61abce86084623ba49303682732e19d91bae90
Artem Manakov <artem.manakov0@gmail.com>
2020-08-17 18:01:43 +0300

    assignment3-task1

--- ДЗ№4.1/ViewController.swift
+++ ДЗ№4.1/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  ДЗ№4.1
 //
-//  Created by Artem Manakov on 17.08.2020.
+//  Created by Artem Manakov on 15.08.2020.
 //  Copyright © 2020 Artem Manakov. All rights reserved.
 //
 
@@ -13,8 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        block4mission1()
     }
+    func block4mission1() {
+        let name = "Artem"
+        print("Меня зовут \(name)")
 
-
+        print("Количество символов в моем имени \(name.count)")
+    }
 }
+
 
